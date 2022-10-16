6fee31467c4e699762878e8ec7b90dc99f3eef4c
Natalya Molchanova <molchanova.nathalie@yandex.ru>
2022-10-16 21:28:52 +0300

    add 002

---
+++ 002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задать массив из 12 элементов, заполненных числами из [0,9]. 
+//Найти сумму положительных/отрицательных элементов массива
+
+int[] array = new int[12];
+int sumPlus = 0;
+int sumMinus = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(- 10, 10);
+    Console.Write(array[i] + " ");
+    if(array[i] >= 0) sumPlus = array[i] + sumPlus;
+    else sumMinus = array[i] + sumMinus;
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма положительных элементов массива равна {sumPlus}");
+Console.WriteLine($"Сумма отрицательных элементов массива равна {sumMinus}");
